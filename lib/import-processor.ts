4287e90e45887e5c9f838d2496b7ec3e95dbe734
patrick <patrick.pircher@ibm.com>
2020-11-06 12:57:08 +0100

    fix

--- lib/import-processor.ts
+++ lib/import-processor.ts
@@ -91,7 +91,7 @@ const importProcessors = {
         }
         if (importPath.startsWith('ui/') || importPath === 'ui') {
           importPath = importPath.replace(/^ui\//, this.options.namespace + '/ui/');
-          importPath = importPath.replace(/^ui$/, this.options.namespace + '/ui/');
+          importPath = importPath.replace(/^ui$/, this.options.namespace + '/ui');
         }
         if (importPath.startsWith('.')) {
           importPath = path.join(path.dirname(relativePath), importPath).split(path.sep).join('/');
