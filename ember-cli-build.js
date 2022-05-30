70c036c5fba2f039311339b026c2190bb982cc1b
patrick <pa.trickjlp@gmail.com>
2022-05-30 11:56:25 +0200

    fix

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,7 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   const app = new EmberAddon(defaults, {
-    // sassOptions: { implementation: require('node-sass') }
+    sassOptions: { implementation: require('node-sass') }
   });
 
   /*
@@ -13,6 +13,5 @@ module.exports = function(defaults) {
     This build file does *not* influence how the addon or the app using it
     behave. You most likely want to be modifying `./index.js` or app's build file
   */
-
   return app.toTree();
 };
