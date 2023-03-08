7167569f0f0c5998881c98401e215fa05d695e36
Patrick Pircher <patricklx@users.noreply.github.com>
2023-03-08 14:41:17 +0100

    Update hbs-imports-babel-plugin.ts

--- lib/hbs-imports-babel-plugin.ts
+++ lib/hbs-imports-babel-plugin.ts
@@ -1,44 +1,44 @@
-const ImportProcessor = require('ember-hbs-imports/lib/import-processor');
-const p = require('path');
-const fs = require('fs');
+import ImportProcessor from './import-processor';
+import p from 'path';
+import type * as BabelCoreNamespace from '@babel/core';
+import type * as BabelTypesNamespace from '@babel/types';
+import type { NodePath } from '@babel/traverse';
+import { PluginObj } from '@babel/core';
+import { V8IntrinsicIdentifier } from '@babel/types';
 
-console.log('loading hbs-imports-babel-plugin.js');
+export type Babel = typeof BabelCoreNamespace;
+export type BabelTypes = typeof BabelTypesNamespace;
 
-module.exports = function hbsImports({ types: t }) {
-  console.log('hbsImports');
+module.exports = function hbsImports({ types: t }: { types: BabelTypes}) {
   let allImports;
   let isTemplateImports;
   let relativePath;
   return {
     name: 'hbs-imports',
     visitor: {
-      /**
-       *
-       * @param path {NodePath}
-       * @constructor
-       */
       Program: {
         enter(path, state) {
           allImports = null;
           isTemplateImports = null;
           let cwd = process.cwd();
-          ImportProcessor.default.options = Object.assign({}, state.opts);
+          ImportProcessor.options = Object.assign({}, state.opts);
+          // @ts-ignore
           const fileName = path.hub.file.opts.filename;
           if (fileName.includes('embroider')) {
             const parts = fileName.split('/');
             const index = parts.indexOf('embroider');
             cwd = parts.slice(0, index + 2).join('/');
           }
-          ImportProcessor.default.options.root = cwd;
-          relativePath = p.relative(ImportProcessor.default.options.root, fileName);
+          ImportProcessor.options.root = cwd;
+          relativePath = p.relative(ImportProcessor.options.root, fileName);
         },
         exit(path, state) {
-          const cwd = ImportProcessor.default.options.root;
+          // @ts-ignore
           const fileName = path.hub.file.opts.filename;
           const importedStyles = allImports && [...allImports.others]
             .filter(x => x.endsWith('.scss'))
-            .map(x => x.replace(new RegExp('^'+ImportProcessor.default.options.namespace + '\/'), ''))
-            .map(x => p.relative(p.dirname(fileName), p.join(cwd, x)))
+            .map(x => x.replace(new RegExp('^'+ImportProcessor.options.namespace + '\/'), ''))
+            .map(x => p.relative(p.dirname(fileName), p.join(ImportProcessor.options.root, x)))
             .map(x => x.startsWith('.') ? x : `./${x}`);
           importedStyles?.forEach((s) => {
             const newImport = t.importDeclaration([], t.stringLiteral(s));
@@ -68,40 +68,30 @@ module.exports = function hbsImports({ types: t }) {
               paths.add(path);
             });
           }
-          const namespaced = new RegExp('^'+ImportProcessor.default.options.namespace + '\/');
           [...paths]
-            .map(x => {
-              if (x.startsWith('.')) {
-                x = p.relative(p.dirname(fileName), p.join(cwd, x));
-                x = x.startsWith('.') ? x : `./${x}`;
-              }
-              if (x.startsWith(ImportProcessor.default.options.namespace + '/')) {
-                x = x.replace(namespaced, '');
-                x = p.relative(p.dirname(fileName), p.join(cwd, x));
-                x = x.startsWith('.') ? x : `./${x}`;
-              }
-              return x;
-            })
+            .map(x => x.replace(new RegExp('^'+ImportProcessor.options.namespace + '\/'), ''))
+            .map(x => p.relative(p.dirname(fileName), p.join(cwd, x)))
+            .map(x => x.startsWith('.') ? x : `./${x}`)
             .forEach((s) => {
-              const newImport = t.importDeclaration([], t.stringLiteral(s));
-              path.node.body.unshift(newImport);
-            })
+            const newImport = t.importDeclaration([], t.stringLiteral(s));
+            path.node.body.unshift(newImport);
+          })
         }
       },
-      ImportDeclaration(path, state) {
+      ImportDeclaration(path) {
         if (path.node.source.value === 'ember-template-imports') {
           isTemplateImports = true;
         }
       },
-      CallExpression(path, state) {
+      CallExpression(path) {
         const call = path.node;
         if (isTemplateImports) return;
-        if (call.callee.name === 'precompileTemplate') {
-          const [content, imported] = ImportProcessor.default.processAst(call.arguments[0].value, relativePath);
+        if ((call.callee as V8IntrinsicIdentifier).name === 'precompileTemplate' && call.arguments[0].type === 'StringLiteral') {
+          const [content, imported] = ImportProcessor.processAst(call.arguments[0].value, relativePath);
           allImports = imported;
           call.arguments[0].value = content;
         }
       }
     }
-  };
+  } as PluginObj;
 }
