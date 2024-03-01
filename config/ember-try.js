2ad3fe59787dcdd3805842db6d709c9b033d02de
Patrick Pircher <patricklx@users.noreply.github.com>
2024-03-01 11:04:35 +0100

    fix config

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,147 +3,76 @@
 const getChannelURL = require('ember-source-channel-url');
 
 module.exports = function() {
-
   return Promise.all([
-
     getChannelURL('release'),
-
     getChannelURL('beta'),
-
     getChannelURL('canary')
-
   ]).then((urls) => {
-
     return {
-
+      usePnpm: true,
       scenarios: [
-
         {
-
           name: 'ember-lts-3.12',
-
           env: {
-
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-
           },
-
           npm: {
-
             devDependencies: {
-
               '@ember/jquery': '^0.5.1',
-
               'ember-source': '~3.12.0'
-
             }
-
           }
-
         },
-
         {
-
           name: 'ember-lts-2.18',
-
           env: {
-
             EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-
           },
-
           npm: {
-
             devDependencies: {
-
               '@ember/jquery': '^0.5.1',
-
               'ember-source': '~2.18.0'
-
             }
-
           }
-
         },
-
         {
-
           name: 'ember-release',
-
           npm: {
-
             devDependencies: {
-
               'ember-source': urls[0]
-
             }
-
           }
-
         },
-
         {
-
           name: 'ember-beta',
-
           npm: {
-
             devDependencies: {
-
               'ember-source': urls[1]
-
             }
-
           }
-
         },
-
         {
-
           name: 'ember-canary',
-
           npm: {
-
             devDependencies: {
-
               'ember-source': urls[2]
-
             }
-
           }
-
         },
-
         {
-
           name: 'ember-default-with-jquery',
-
           env: {
-
             EMBER_OPTIONAL_FEATURES: JSON.stringify({
-
               'jquery-integration': true
-
             })
-
           },
-
           npm: {
-
             devDependencies: {
-
               '@ember/jquery': '^0.5.1'
-
             }
-
           }
-
         }
-
       ]
-
     };
-
   });
-
 };
